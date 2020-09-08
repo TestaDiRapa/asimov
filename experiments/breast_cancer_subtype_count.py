6a642471aed664879a3fca79045613a5b27fe7ae
TestaDiRapa <v.c.pierro@gmail.com>
2020-09-08 16:21:29 +0200

    Added some experimental script files

---
+++ experiments/breast_cancer_subtype_count.py
@@ -0,0 +1,16 @@
+from dataset import identify_breast_cancer_subtype
+import os
+
+statuses = {
+    "LuminalA": 0,
+    "LuminalB": 0,
+    "TNBS": 0,
+    "HER2+": 0,
+    "Unclear": 0
+}
+for dir_, _, files in os.walk("../data/breast_clinical"):
+    for file in files:
+        if file[-3:] == "xml":
+            path = os.path.join(dir_, file)
+            statuses[identify_breast_cancer_subtype(path)] += 1
+print(statuses)
