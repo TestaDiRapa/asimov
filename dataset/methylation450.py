47b76e0d14c098be64849b11ddbfe7ed2c6cfbaa
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2020-09-08 19:07:10 +0200

    Bug fixing

--- dataset/methylation450.py
+++ dataset/methylation450.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import re
 
 
 def filter_cpg_islands(folder):
@@ -14,12 +15,11 @@ def filter_cpg_islands(folder):
     files_counter = 0
     for dir_, _, files in os.walk(folder):
         for file in files:
-            if file[-3:] == ".txt":
+            if re.search(r'^jhu-usc\..+txt$', file):
+                print(file)
                 files_counter += 1
                 dataset = pd.read_csv(os.path.join(dir_, file), sep='\t', na_values="NA")
-                print(dataset.size)
                 dataset = dataset[["Composite Element REF", "Beta_value"]].dropna()
-                print(dataset.size)
                 for cpg in dataset.iloc[:, 0]:
                     if cpg in islands:
                         islands[cpg] += 1
