c143560df5a979e4fd341139aadfcbdd9c9cecb5
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2020-09-09 18:45:08 +0200

    Updated dataset test

--- tests/breast_common_cpg_islands.py
+++ tests/breast_common_cpg_islands.py
@@ -1,11 +1,12 @@
-from dataset.methylation450 import filter_cpg_islands
+# from dataset.methylation450 import filter_cpg_islands
+from dataset.methylation450 import create_methylation_dataset
 import pickle
 
 # First test to create a training dataset for the autoencoder
 
-islands, num_files = filter_cpg_islands(["../data/breast_methylation_450", "../data/lung_methylation_450"])
-pickle.dump(islands, open("../data/breast_and_lung.pkl", "wb"))
-# islands, num_files = pickle.load(open("../data/breast_methylation_450.pkl", "rb")), 893
+# islands, num_files = filter_cpg_islands(["../data/breast_methylation_450", "../data/lung_methylation_450"])
+# pickle.dump(islands, open("../data/breast_and_lung.pkl", "wb"))
+islands, num_files = pickle.load(open("../data/breast_and_lung.pkl", "rb")), 893+920
 count = {
     "100": 0,
     "99-90": 0,
@@ -20,10 +21,14 @@ count = {
     "9-0": 0
 }
 
+cpg_islands = []
+
 for cpg, num_instances in islands.items():
     percentage = num_instances*100/num_files
     if percentage == 100:
         count["100"] += 1
+        if cpg[:2] == "cg":
+            cpg_islands.append(cpg)
     elif percentage >= 90:
         count["99-90"] += 1
     elif percentage >= 80:
@@ -44,4 +49,10 @@ for cpg, num_instances in islands.items():
         count["19-10"] += 1
     else:
         count["9-0"] += 1
+print(cpg_islands)
 print(count)
+print(len(cpg_islands))
+
+dataset = create_methylation_dataset(["../data/breast_methylation_450", "../data/lung_methylation_450"], cpg_islands)
+pickle.dump(dataset, open("../data/breast_and_lung_pd.pkl", "wb"))
+dataset.to_csv("../data/breast_and_lung_pd.csv", sep='\t')
