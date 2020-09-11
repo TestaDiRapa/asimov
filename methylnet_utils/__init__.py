ee0e83f8e84c9bb6af71fbcf4b234f890334f60d
TestaDiRapa <v.c.pierro@gmail.com>
2020-09-11 16:09:59 +0200

    Added function to split dataset

--- methylnet_utils/__init__.py
+++ methylnet_utils/__init__.py
@@ -1,7 +1,9 @@
 from dataset import identify_breast_cancer_subtype
 from dataset.methylation450 import folder_generator
+import os
 import pandas as pd
 import pickle
+import random
 
 
 def generate_subtype_methylation_array(clinical_folder, methylation_dataset, output_filename):
@@ -22,9 +24,41 @@ def generate_subtype_methylation_array(clinical_folder, methylation_dataset, out
     pheno = pd.DataFrame(index=barcodes, columns=["subtype"])
     for barcode in barcodes:
         if barcode.split('-')[3][:2] == "11":
-            pheno.loc[barcode] = "control"
+            pheno.loc[barcode] = "Control"
         else:
             subject_barcode = '-'.join(barcode.split('-')[:3])
             pheno.loc[barcode] = subtypes[subject_barcode]
     pheno = pheno.dropna()
     pickle.dump({"beta": beta, "pheno": pheno}, open(output_filename, "wb"))
+
+
+def split_methylation_array_by_pheno(methylation_array_filename, pheno_column, output_folder, val_rate=0.1,
+                                     test_rate=0.1):
+    methylation_array = pickle.load(open(methylation_array_filename, "rb"))
+    beta = methylation_array["beta"]
+    pheno = methylation_array["pheno"]
+
+    train_barcodes, test_barcodes, val_barcodes = [], [], []
+    for class_ in pheno[pheno_column].unique():
+        barcodes = pheno[pheno[pheno_column] == class_].index.to_list()
+        val_samples = int(len(barcodes)*val_rate)
+        test_samples = int(len(barcodes)*test_rate)
+        samples = random.sample(barcodes, val_samples+test_samples)
+        val_tmp = samples[:val_samples]
+        test_tmp = samples[val_samples:]
+        for b in barcodes:
+            if b in val_tmp:
+                val_barcodes.append(b)
+            elif b in test_tmp:
+                test_barcodes.append(b)
+            else:
+                train_barcodes.append(b)
+
+    pickle.dump({"beta": beta.loc[train_barcodes], "pheno": pheno.loc[train_barcodes]},
+                open(os.path.join(output_folder, "train"+methylation_array_filename), "wb"))
+
+    pickle.dump({"beta": beta.loc[test_barcodes], "pheno": pheno.loc[test_barcodes]},
+                open(os.path.join(output_folder, "test"+methylation_array_filename), "wb"))
+
+    pickle.dump({"beta": beta.loc[val_barcodes], "pheno": pheno.loc[val_barcodes]},
+                open(os.path.join(output_folder, "val"+methylation_array_filename), "wb"))
