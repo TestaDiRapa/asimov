ef078a7c4c3de4a58ea5851bdabf183b4528b326
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2020-09-12 12:30:11 +0200

    Added filters to datset creation

--- dataset/methylation450.py
+++ dataset/methylation450.py
@@ -3,18 +3,22 @@ import os
 import re
 
 
-def create_methylation_dataset(folders, islands):
+def create_methylation_dataset(folders, islands, filters=dict()):
     """
     The function creates a pandas Dataframe where each row is a sample and each column is a CpG island. Only the islands
     passed as parameter are considered.
     :param folders: a list of string containing the folders where the files are
     :param islands: a list of CpG island
+    :param filters: a dict where the key is the column and the value the value used to filter
     :return: a pandas Dataframe
     """
     new_dataset = pd.DataFrame(columns=islands+["barcode"])
     for path in folder_generator(folders, r'^jhu-usc\..+txt$'):
         dataset = pd.read_csv(path, sep='\t', na_values="NA", index_col=0)
-        dataset = dataset[["Beta_value"]].dropna().loc[islands].T.reset_index().drop("index", axis=1)
+        dataset = dataset.dropna().loc[islands]
+        for col, value in filters.items():
+            dataset = dataset[dataset[col] == value]
+        dataset = dataset[["Beta_value"]].T.reset_index().drop("index", axis=1)
         dataset.columns.name = None
         barcode = re.search(r'TCGA-[A-Z0-9]{2}-[A-Z0-9]{4}-[A-Z0-9]{3}-[A-Z0-9]{3}-[A-Z0-9]{4}-[A-Z0-9]{2}', path).group()
         dataset["barcode"] = barcode
