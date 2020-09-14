9db0a4cdca268eda288d57e95f1722c65d136205
TestaDiRapa <v.c.pierro@gmail.com>
2020-09-14 17:24:10 +0200

    First test

--- models/classifiers.py
+++ models/classifiers.py
@@ -1,5 +1,10 @@
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices("GPU")
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.layers import *
+from tensorflow.keras.metrics import BinaryCrossentropy
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 import os
@@ -10,7 +15,7 @@ class NeuralClassifier:
     def __init__(self, input_shape, dropout_rate=0.2, model_serialization_path="models/classifier/",
                  model_name="classifier"):
         self.model = None
-        os.makedirs(model_serialization_path)
+        # os.makedirs(model_serialization_path)
         self.serialization_path = model_serialization_path
         self.model_name = model_name
         self.generate_model(input_shape, dropout_rate)
@@ -21,7 +26,7 @@ class NeuralClassifier:
         hl_2 = Dense(300, activation="relu")(hl_1)
         hl_3 = Dense(3000, activation="relu")(hl_2)
         hl_d = Dropout(dropout_rate)(hl_3)
-        output_layer = Dense(5, activation="sigmoid")(hl_d)
+        output_layer = Dense(6, activation="sigmoid")(hl_d)
 
         self.model = Model(inputs=input_layer, outputs=output_layer)
         # self.model.save(os.path.join(self.serialization_path, self.model_name+".model"))
@@ -29,7 +34,7 @@ class NeuralClassifier:
         optimizer = Adam(lr=0.001)
         self.model.compile(optimizer=optimizer,
                            loss="binary_crossentropy",
-                           metrics=["accuracy", "loss"])
+                           metrics=["accuracy"])
 
         self.model.summary()
 
@@ -38,7 +43,7 @@ class NeuralClassifier:
             training_set,
             # batch_size=batch_size,
             epochs=epochs,
-            callbacks=[EarlyStopping(monitor='loss', min_delta=0.1, patience=10)],
+            # callbacks=[EarlyStopping(monitor="val_loss", min_delta=0.1, patience=10)],
             validation_data=validation_set
         )
         # self.model.save_weights(os.path.join(self.serialization_path, self.model_name+".weights"))
--- tests/train_classifier.py
+++ tests/train_classifier.py
@@ -1,8 +1,8 @@
 from models import MethylationArrayGenerator
 from models.classifiers import NeuralClassifier
 
-training_set = MethylationArrayGenerator("../data/breast_embedded/train_oe.pkl", "subtype")
-validation_set = MethylationArrayGenerator("../data/breast_embedded/val_oe.pkl", "subtype")
+training_set = MethylationArrayGenerator("../data/breast_embedded/train_oe.pkl", "subtype", batch_size=8)
+validation_set = MethylationArrayGenerator("../data/breast_embedded/val_oe.pkl", "subtype", batch_size=8)
 
 nn = NeuralClassifier(100, model_serialization_path="../data/models/classifier/")
-nn.fit(training_set, validation_set, 500)
+nn.fit(training_set, validation_set, 200)
