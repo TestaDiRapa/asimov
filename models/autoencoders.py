a795d62192aa082a3024939009fb7ee9c1031e3c
TestaDiRapa <v.c.pierro@gmail.com>
2020-09-17 15:36:58 +0200

    Added function to embed a methylation array

--- models/autoencoders.py
+++ models/autoencoders.py
@@ -1,9 +1,11 @@
 from abc import abstractmethod
 from models import AbstractModel
+from sklearn import preprocessing
 from tensorflow.keras.layers import *
 from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
+import pandas as pd
 
 
 class AbstractAutoencoder(AbstractModel):
@@ -80,19 +82,35 @@ class AbstractAutoencoder(AbstractModel):
         """
         return self.__encoder.predict(x_input)
 
+    def encode_methylation_array(self, methylation_array):
+        """
+        This function creates an embedded representation of a methylation array
+        :param methylation_array: the methylation array to transform
+        :return: another methylation array
+        """
+        scaler = preprocessing.MinMaxScaler()
+        scaled_betas = scaler.fit_transform(methylation_array["beta"].values)
+        print(scaled_betas.shape)
+        embedded_betas = self.__encoder.predict(scaled_betas)
+        print(embedded_betas.shape)
+        new_betas = pd.DataFrame(data=embedded_betas, index=list(methylation_array["pheno"].index.values))
+        print(new_betas.shape)
+        return {"beta": new_betas, "pheno": methylation_array["pheno"]}
+
 
 class MiRNAEncoder(AbstractAutoencoder):
     """
     An autoencoder for mirna data
     """
 
-    def __init__(self,  model_serialization_path="models/autoencoder/", model_name="autoencoder"):
+    def __init__(self,  input_shape, model_serialization_path="models/autoencoder/", model_name="autoencoder"):
         """
         Class constructor
         :param model_serialization_path: the path where to save the model
         :param model_name: the model name
         """
         super().__init__(model_serialization_path, model_name)
+        self.generate_model(input_shape)
 
     def generate_model(self, input_shape):
         """
@@ -105,21 +123,21 @@ class MiRNAEncoder(AbstractAutoencoder):
         hidden_neurons_3 = 300
         latent_dimension = 100
         
-        encoder_input_layer = Input(input_shape=input_shape)
+        encoder_input_layer = Input(shape=input_shape)
         encoder_layer_1 = Dense(hidden_neurons_1, activation="relu")(encoder_input_layer)
         encoder_layer_2 = Dense(hidden_neurons_2, activation="relu")(encoder_layer_1)
         encoder_layer_3 = Dense(hidden_neurons_3, activation="relu")(encoder_layer_2)
         encoder_output = Dense(latent_dimension, activation="sigmoid")(encoder_layer_3)
         self.generate_encoder(encoder_input_layer, encoder_output)
 
-        decoder_input_layer = Input(input_shape=latent_dimension)
+        decoder_input_layer = Input(shape=latent_dimension)
         decoder_layer_1 = Dense(hidden_neurons_3, activation="relu")(decoder_input_layer)
         decoder_layer_2 = Dense(hidden_neurons_2, activation="relu")(decoder_layer_1)
         decoder_layer_3 = Dense(hidden_neurons_1, activation="relu")(decoder_layer_2)
         decoder_output = Dense(input_shape, activation="sigmoid")(decoder_layer_3)
         self.generate_decoder(decoder_input_layer, decoder_output)
 
-        model_input = Input(input_shape=input_shape)
+        model_input = Input(shape=input_shape)
         code = self.encoder_layer(model_input)
         model_output = self.decoder_layer(code)
         self.compile_model(model_input, model_output, Adam(lr=0.001), CategoricalCrossentropy())
