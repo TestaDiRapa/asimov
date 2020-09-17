ca3ee50dc399c1f8a71b4b009fe297b5026cdf79
TestaDiRapa <v.c.pierro@gmail.com>
2020-09-17 10:13:12 +0200

    Updated class hierarchy

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,96 @@
+from abc import ABC, abstractmethod
 from methylnet_utils import split_methylation_array_by_pheno
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.models import Model
 from tensorflow.keras.utils import Sequence
 import numpy as np
+import os
 import pandas as pd
 import pickle
+
+
+class AbstractModel(ABC):
+    """
+    An abstract class to define a common interface for all the models
+    """
+
+    def __init__(self,  model_serialization_path="models/autoencoder/", model_name="autoencoder"):
+        """
+        Class constructor
+        :param model_serialization_path: the path where to save the model
+        :param model_name: the model name
+        """
+        super().__init__()
+        self.__model = None
+        if not os.path.exists(model_serialization_path):
+            os.makedirs(model_serialization_path)
+        self.__serialization_path = model_serialization_path
+        self.__model_name = model_name
+
+    @abstractmethod
+    def generate_model(self, input_shape):
+        """
+        Method that the subclasses must implement, defines the model itself
+        :param input_shape: the input shape
+        :return: None
+        """
+        pass
+
+    def compile_model(self, input_layer, output_layer, optimizer, loss):
+        """
+        This method compiles the method and saves it
+        :param input_layer: the input layer (a keras layer)
+        :param output_layer: the output layer (a keras layer)
+        :param optimizer: the optimizer function
+        :param loss: the loss function
+        :return: None
+        """
+        self.__model = Model(inputs=input_layer, outputs=output_layer)
+
+        self.__model.compile(optimizer=optimizer,
+                             loss=loss,
+                             metrics=["accuracy"])
+        self.__model.save(os.path.join(self.__serialization_path, self.__model_name + ".h5"))
+        self.__model.summary()
+
+    def predict(self, x_test):
+        """
+        Wrapper for the predict method of the model class from keras
+        :param x_test: the test set
+        :return: the predictions
+        """
+        return self.__model.predict(x_test)
+
+    def evaluate(self, x_test, y_test):
+        """
+        This method evaluates the performances of the model on a test set
+        :param x_test: the test set features
+        :param y_test: the test set labels
+        :return: the average accuracy
+        """
+        acc = 0
+        for p, r in zip(self.predict(x_test), y_test):
+            if np.argmax(p) == np.argmax(r):
+                acc += 1
+        return acc/len(y_test)
+
+    def fit(self, training_set, validation_set, epochs, verbose=1):
+        """
+        This method fits the model using the EarlyStopping callback and save the weights
+        :param training_set: the training set
+        :param validation_set:  the validation set
+        :param epochs: the number of epochs
+        :param verbose: 0, 1 or 2 (check tensorflow documentation)
+        :return: None
+        """
+        self.__model.fit(
+            training_set,
+            verbose=verbose,
+            epochs=epochs,
+            callbacks=[EarlyStopping(monitor="val_loss", min_delta=0.05, patience=10)],
+            validation_data=validation_set
+        )
+        self.__model.save_weights(os.path.join(self.__serialization_path, self.__model_name + ".h5"))
 
 
 class MethylationArrayGenerator(Sequence):
--- models/classifiers.py
+++ models/classifiers.py
@@ -1,99 +1,10 @@
-from abc import ABC, abstractmethod
-from tensorflow.keras.callbacks import EarlyStopping
+from models import AbstractModel
 from tensorflow.keras.layers import *
 from tensorflow.keras.losses import CategoricalCrossentropy
-from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
-import numpy as np
-import os
 
 
-class AbstractClassifier(ABC):
-    """
-    An abstract class that define a common fit/prediction interface for all the models
-    """
-
-    def __init__(self,  model_serialization_path="models/classifier/", model_name="classifier"):
-        """
-        Class constructor
-        :param model_serialization_path: the path where to save the model
-        :param model_name: the model name
-        """
-        super().__init__()
-        self.__model = None
-        if not os.path.exists(model_serialization_path):
-            os.makedirs(model_serialization_path)
-        self.__serialization_path = model_serialization_path
-        self.__model_name = model_name
-
-    @abstractmethod
-    def generate_model(self, input_shape):
-        """
-        Method that the subclasses must implement, defines the model itself
-        :param input_shape: the input shape
-        :return: None
-        """
-        pass
-
-    def compile_model(self, input_layer, output_layer, optimizer, loss):
-        """
-        This method compiles the method and saves it
-        :param input_layer: the input layer (a keras layer)
-        :param output_layer: the output layer (a keras layer)
-        :param optimizer: the optimizer function
-        :param loss: the loss function
-        :return: None
-        """
-        self.__model = Model(inputs=input_layer, outputs=output_layer)
-
-        self.__model.compile(optimizer=optimizer,
-                             loss=loss,
-                             metrics=["accuracy"])
-        self.__model.save(os.path.join(self.__serialization_path, self.__model_name + ".h5"))
-        self.__model.summary()
-
-    def fit(self, training_set, validation_set, epochs, verbose=1):
-        """
-        This method fits the model using the EarlyStopping callback and save the weights
-        :param training_set: the training set
-        :param validation_set:  the validation set
-        :param epochs: the number of epochs
-        :param verbose: 0, 1 or 2 (check tensorflow documentation)
-        :return: None
-        """
-        self.__model.fit(
-            training_set,
-            verbose=verbose,
-            # batch_size=batch_size,
-            epochs=epochs,
-            callbacks=[EarlyStopping(monitor="val_loss", min_delta=0.05, patience=10)],
-            validation_data=validation_set
-        )
-        self.__model.save_weights(os.path.join(self.__serialization_path, self.__model_name+".h5"))
-
-    def predict(self, x_test):
-        """
-        Wrapper for the predict method of the model class from keras
-        :param x_test: the test set
-        :return: the predictions
-        """
-        return self.__model.predict(x_test)
-
-    def evaluate(self, x_test, y_test):
-        """
-        This method evaluates the performances of the model on a test set
-        :param x_test: the test set features
-        :param y_test: the test set labels
-        :return: the average accuracy
-        """
-        acc = 0
-        for p, r in zip(self.predict(x_test), y_test):
-            if np.argmax(p) == np.argmax(r):
-                acc += 1
-        return acc/len(y_test)
-
-
-class NeuralClassifier(AbstractClassifier):
+class NeuralClassifier(AbstractModel):
     """
     Implements the abstract model as a fully connected NN with 3 hidden layers
     """
@@ -127,7 +38,7 @@ class NeuralClassifier(AbstractClassifier):
         self.compile_model(input_layer, output_layer, Adam(lr=0.001), CategoricalCrossentropy())
 
 
-class ConvolutionalClassifier(AbstractClassifier):
+class ConvolutionalClassifier(AbstractModel):
     """
     Implements the abstract model as a convolutional neural network
     """
