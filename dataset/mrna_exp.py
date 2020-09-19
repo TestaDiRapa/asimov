d1bb520c43f503fcbce8d55507e54af198067926
TestaDiRapa <v.c.pierro@gmail.com>
2020-09-19 16:57:25 +0200

    Small fix

--- dataset/mrna_exp.py
+++ dataset/mrna_exp.py
@@ -15,7 +15,7 @@ def mrna_dataset_creator(mrna_folder, genes):
     new_dataset = pd.DataFrame(columns=genes + ["barcode"])
     for path in folder_generator(mrna_folder, r'FPKM\.txt\.gz$'):
         dataset = pd.read_csv(path, compression="gzip", sep='\t', index_col=0, header=None, na_values="NA")
-        dataset = dataset.dropna().loc[genes]
+        dataset = dataset.dropna().loc[genes].T
         dataset.columns.name = None
         dataset["barcode"] = finder.find_barcode(path.split(os.sep)[-2])
         new_dataset = new_dataset.append(dataset)
