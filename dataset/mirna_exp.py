ba7453000e48bc3409e73b08398c62cd142ca2f7
TestaDiRapa <v.c.pierro@gmail.com>
2020-09-19 13:04:09 +0200

    Refactored

--- dataset/mirna_exp.py
+++ dataset/mirna_exp.py
@@ -1,10 +1,6 @@
-from dataset import folder_generator
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.firefox.options import Options
+from dataset import BarcodeFinder, folder_generator
 import os
 import pandas as pd
-import time
 
 
 def mirna_dataset_creator(mirna_folder, mirnas):
@@ -15,30 +11,14 @@ def mirna_dataset_creator(mirna_folder, mirnas):
     :param mirnas: a list of mirna to consider
     :return: a dictionary
     """
-    driver_options = Options()
-    driver_options.headless = True
-    driver = webdriver.Firefox(options=driver_options)
-    driver.maximize_window()
     new_dataset = pd.DataFrame(columns=mirnas + ["barcode"])
+    finder = BarcodeFinder()
     for folder in folder_generator(mirna_folder, r'mirnas\.quantification\.txt$'):
         dataset = pd.read_csv(folder, sep='\t', na_values="NA", index_col=0)
         dataset = dataset[["reads_per_million_miRNA_mapped"]].dropna().loc[mirnas].T.reset_index().drop("index", axis=1)
         dataset.columns.name = None
-        url = "https://portal.gdc.cancer.gov/files/{}".format(folder.split(os.sep)[-2])
-        timeout_flag = True
-        while timeout_flag:
-            try:
-                driver.get(url)
-                css_selector = "a.unnamed-link[href*='bioId']"
-                time.sleep(3)
-                barcode = driver.find_element_by_css_selector(css_selector).text
-                timeout_flag = False
-            except TimeoutException:
-                driver.quit()
-                driver = webdriver.Firefox(options=driver_options)
-                driver.maximize_window()
-        dataset["barcode"] = barcode
+        dataset["barcode"] = finder.find_barcode(folder.split(os.sep)[-2])
         new_dataset = new_dataset.append(dataset)
-    driver.quit()
+    finder.quit()
     new_dataset = new_dataset.set_index("barcode")
     return new_dataset
