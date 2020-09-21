717f86214a743440016d2a21d947f3d2efa394bc
TestaDiRapa <v.c.pierro@gmail.com>
2020-09-21 09:58:01 +0200

    Added flag to use file uuid as barcode

--- dataset/mirna_exp.py
+++ dataset/mirna_exp.py
@@ -3,12 +3,13 @@ import os
 import pandas as pd
 
 
-def mirna_dataset_creator(mirna_folder, mirnas):
+def mirna_dataset_creator(mirna_folder, mirnas, barcode = True):
     """
     This function creates a dictionary where the keys are the mirna filenames and the values the barcodes of the
     subjects
     :param mirna_folder: the folder that contains all the mirna files
     :param mirnas: a list of mirna to consider
+    :param barcode: boolean flag to search the barcode on GDC or use the uuid instead
     :return: a dictionary
     """
     new_dataset = pd.DataFrame(columns=mirnas + ["barcode"])
@@ -17,7 +18,10 @@ def mirna_dataset_creator(mirna_folder, mirnas):
         dataset = pd.read_csv(folder, sep='\t', na_values="NA", index_col=0)
         dataset = dataset[["reads_per_million_miRNA_mapped"]].dropna().loc[mirnas].T.reset_index().drop("index", axis=1)
         dataset.columns.name = None
-        dataset["barcode"] = finder.find_barcode(folder.split(os.sep)[-2])
+        if barcode:
+            dataset["barcode"] = finder.find_barcode(folder.split(os.sep)[-2])
+        else:
+            dataset["barcode"] = folder.split(os.sep)[-2]
         new_dataset = new_dataset.append(dataset)
     finder.quit()
     new_dataset = new_dataset.set_index("barcode")
--- dataset/mrna_exp.py
+++ dataset/mrna_exp.py
@@ -3,21 +3,25 @@ import os
 import pandas as pd
 
 
-def mrna_dataset_creator(mrna_folder, genes):
+def mrna_dataset_creator(mrna_folders, genes, barcode=True):
     """
     This function creates a dictionary where the keys are the mRBA filenames and the values the barcodes of the
     subjects
-    :param mrna_folder: the folder that contains all the mRNA files
+    :param mrna_folders: the folder or a list of folders that contains all the mRNA files
     :param genes: a list of transcripts to consider
+    :param barcode: boolean flag to search the barcode on GDC or use the uuid instead
     :return: a dictionary
     """
     finder = BarcodeFinder()
     new_dataset = pd.DataFrame(columns=genes + ["barcode"])
-    for path in folder_generator(mrna_folder, r'FPKM\.txt\.gz$'):
+    for path in folder_generator(mrna_folders, r'FPKM\.txt\.gz$'):
         dataset = pd.read_csv(path, compression="gzip", sep='\t', index_col=0, header=None, na_values="NA")
         dataset = dataset.dropna().loc[genes].T
         dataset.columns.name = None
-        dataset["barcode"] = finder.find_barcode(path.split(os.sep)[-2])
+        if barcode:
+            dataset["barcode"] = finder.find_barcode(path.split(os.sep)[-2])
+        else:
+            dataset["barcode"] = path.split(os.sep)[-2]
         new_dataset = new_dataset.append(dataset)
     finder.quit()
     new_dataset = new_dataset.set_index("barcode")
