207ea4be9fd868afa9941d63e1ff9c93f6b77465
TestaDiRapa <v.c.pierro@gmail.com>
2020-09-28 19:40:37 +0200

    Added logging

--- scripts/train_mirna_classification.py
+++ scripts/train_mirna_classification.py
@@ -2,10 +2,18 @@ from dataset import filter_expression_by_rate
 from models import methylation_array_kcv
 from models.autoencoders import MiRNAEncoder
 from models.benchmark import benchmark_svm, benchmark_rf, benchmark_knn
-from models.classifiers import NeuralClassifier, ConvolutionalClassifier
+from models.classifiers import NeuralClassifier, ConvolutionalClassifier, MOLIClassifier
 from models.generators import AutoencoderGenerator
 from tensorflow.keras.callbacks import EarlyStopping
 import pickle
+import os
+
+logfile_name = "../data/mirna_embedding.tsv"
+if not os.path.exists(logfile_name):
+    with open(logfile_name, "w") as logfile:
+        fields = ["MODEL", "ACCURACY_EVALUATION_METHOD", "LATENT_DIMENSION", "VALIDATION_ACCURACY", "TEST_ACCURACY",
+                  '\n']
+        logfile.write('\t'.join(fields))
 
 # Define the latent dimension
 ld = 200
@@ -33,13 +41,24 @@ pickle.dump(mirna_dataset, open("../data/mirna_embedded.pkl", "wb"))
 
 # Classification with ML and DL models
 params = {"input_shape": mirna_dataset["beta"].shape[1], "model_serialization_path": "../data/models/classifier/",
-          "dropout_rate": 0.2, "output_shape": len(mirna_dataset["pheno"]["subtype"].unique())}
+          "dropout_rate": 0.05, "output_shape": len(mirna_dataset["pheno"]["subtype"].unique())}
 val_res, test_res = methylation_array_kcv(mirna_dataset,
-                                          NeuralClassifier,
+                                          MOLIClassifier,
                                           params,
                                           "subtype",
                                           callbacks=[EarlyStopping(monitor="val_loss", min_delta=0.05, patience=10)])
+
 print("Validation accuracy: {} - Test accuracy: {}".format(val_res, test_res))
-print("SVM validation accuracy: {} - SVM test accuracy: {}".format(*benchmark_svm(mirna_dataset, "subtype")))
-print("KNN validation accuracy: {} - KNN test accuracy: {}".format(*benchmark_knn(mirna_dataset, "subtype")))
-print("RF validation accuracy: {} - RF test accuracy: {}".format(*benchmark_rf(mirna_dataset, "subtype")))
+svm_val, svm_test = benchmark_svm(mirna_dataset, "subtype")
+print("SVM validation accuracy: {} - SVM test accuracy: {}".format(svm_val, svm_test))
+knn_val, knn_test = benchmark_knn(mirna_dataset, "subtype")
+print("KNN validation accuracy: {} - KNN test accuracy: {}".format(knn_val, knn_test))
+rf_val, rf_test = benchmark_rf(mirna_dataset, "subtype")
+print("RF validation accuracy: {} - RF test accuracy: {}".format(rf_val, rf_test))
+
+with open(logfile_name, 'a') as logfile:
+    base = "{}\t{}\t{}\t{}\t{}\n"
+    logfile.write(base.format(str(mirna_encoder), "MLP", ld, val_res, test_res))
+    logfile.write(base.format(str(mirna_encoder), "SVM", ld, svm_val, svm_test))
+    logfile.write(base.format(str(mirna_encoder), "KNN", ld, knn_val, knn_test))
+    logfile.write(base.format(str(mirna_encoder), "RF", ld, rf_val, rf_test))
