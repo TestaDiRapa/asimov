1c13466627df1083cc6e5bc35e19d142672d3556
TestaDiRapa <v.c.pierro@gmail.com>
2020-09-28 18:10:53 +0200

    Added MOLIClassifier and str magic method

--- models/classifiers.py
+++ models/classifiers.py
@@ -6,7 +6,7 @@ from tensorflow.keras.optimizers import Adam
 
 class NeuralClassifier(AbstractModel):
     """
-    Implements the abstract model as a fully connected NN with 3 hidden layers
+    Implements the abstract model as a fully connected NN with 1 hidden layers
     """
 
     def __init__(self, input_shape, output_shape, dropout_rate=0.2, model_serialization_path="models/classifier/",
@@ -40,6 +40,12 @@ class NeuralClassifier(AbstractModel):
         output_layer = Dense(output_shape, activation="sigmoid")(hl_d)
 
         self.compile_model(input_layer, output_layer, Adam(lr=0.001), CategoricalCrossentropy())
+
+    def __str__(self):
+        """
+        :return: string representation
+        """
+        return "NeuralClassifier"
 
 
 class ConvolutionalClassifier(AbstractModel):
@@ -105,3 +111,51 @@ class ConvolutionalClassifier(AbstractModel):
         output_layer = Dense(output_shape, activation="sigmoid")(drop_2)
 
         self.compile_model(input_layer, output_layer, Adam(lr=0.001), CategoricalCrossentropy())
+
+    def __str__(self):
+        """
+        :return: string representation
+        """
+        return "ConvolutionalClassifier"
+
+
+class MOLIClassifier(AbstractModel):
+    """
+    Implements the abstract model as a fully connected NN with 3 hidden layers
+    """
+
+    def __init__(self, input_shape, output_shape, dropout_rate=0.2, model_serialization_path="models/classifier/",
+                 model_name="classifier"):
+        """
+        Class constructor
+        :param input_shape: the input shape as int
+        :param output_shape: the output shape as int
+        :param dropout_rate: the dropout rate
+        :param model_serialization_path: the path where to save the model
+        :param model_name: the model name
+        """
+        super().__init__(model_serialization_path, model_name)
+        self.dropout_rate = dropout_rate
+        self.generate_model(input_shape, output_shape)
+
+    def generate_model(self, input_shape, output_shape):
+        """
+        Generates the model
+        :param input_shape: the input shape
+        :param output_shape: the output shape
+        :return: None
+        """
+        input_layer = Input(shape=input_shape)
+        relu_1 = Dense(32, activation="relu")(input_layer)
+        drop_1 = Dropout(self.dropout_rate)(relu_1)
+        relu_2 = Dense(32, activation="relu")(drop_1)
+        drop_2 = Dropout(self.dropout_rate)(relu_2)
+        output_layer = Dense(output_shape, activation="sigmoid")(drop_2)
+
+        self.compile_model(input_layer, output_layer, Adam(lr=0.001), CategoricalCrossentropy())
+
+    def __str__(self):
+        """
+        :return: string representation
+        """
+        return "MOLIClassifier"
