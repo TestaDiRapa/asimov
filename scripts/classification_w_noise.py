88f4b13b5b3a418275aa7fadfe5cb7daf3a820bb
TestaDiRapa <v.c.pierro@gmail.com>
2020-10-01 19:17:15 +0200

    Optimized code

--- scripts/classification_w_noise.py
+++ scripts/classification_w_noise.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from tensorflow.keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pickle
@@ -14,13 +15,13 @@ import pickle
 # SOME FUNCTIONS
 def train_autoencoder(methylation_array, latent_dimension):
     val_size = int(methylation_array["beta"].shape[0] * 0.1)
-    validation_set = AutoencoderGenerator(methylation_array["beta"].iloc[:val_size, :])
-    training_set = AutoencoderGenerator(methylation_array["beta"].iloc[val_size:, :])
+    val_set = AutoencoderGenerator(methylation_array["beta"].iloc[:val_size, :])
+    train_set = AutoencoderGenerator(methylation_array["beta"].iloc[val_size:, :])
 
     # Autoencoder training
     encoder = MiRNAEncoder(methylation_array["beta"].shape[1], latent_dimension=latent_dimension,
                            model_serialization_path="../data/models/")
-    encoder.fit(training_set, validation_set, 500,
+    encoder.fit(train_set, val_set, 500,
                 callbacks=[EarlyStopping(monitor="val_loss", min_delta=0.05, patience=10)])
     return encoder
 
@@ -42,33 +43,83 @@ def correct_labels(methylation_array):
     return methylation_array
 
 
-def train_dnn_classifier(classifier, training_set, validation_set, test_set):
-    params = {"input_shape": training_set["beta"].shape[1], "model_serialization_path": "../data/models/classifier/",
-              "dropout_rate": 0.3, "output_shape": len(training_set["pheno"]["subtype"].unique())}
+def train_dnn_classifier(classifier, train_set, val_set, test_data):
+    params = {"input_shape": train_set["beta"].shape[1], "model_serialization_path": "../data/models/classifier/",
+              "dropout_rate": 0.3, "output_shape": len(train_set["pheno"]["subtype"].unique())}
     model = classifier(**params)
-    model.fit(MethylationArrayGenerator(training_set, "subtype"),
-              MethylationArrayGenerator(validation_set, "subtype"),
+    model.fit(MethylationArrayGenerator(train_set, "subtype"),
+              MethylationArrayGenerator(val_set, "subtype"),
               500,
               verbose=0,
               callbacks=[EarlyStopping(monitor="val_loss", min_delta=0.05, patience=20)])
-    test_accuracy = model.evaluate(test_set["beta"].to_numpy(),
-                                   pd.get_dummies(test_set["pheno"]["subtype"]).to_numpy())
+    test_accuracy = model.evaluate(test_data["beta"].to_numpy(),
+                                   pd.get_dummies(test_data["pheno"]["subtype"]).to_numpy())
     return model, test_accuracy
 
 
-def train_ml_classifier(training_set, test_set, model, params, validation_set=None):
-    if validation_set is not None:
-        training_set["beta"] = training_set["beta"].append(validation_set["beta"])
-        training_set["pheno"] = training_set["pheno"].append(validation_set["pheno"])
+def train_ml_classifier(train_set, test_data, model, params, val_set=None):
+    if val_set is not None:
+        train_set["beta"] = train_set["beta"].append(val_set["beta"])
+        train_set["pheno"] = train_set["pheno"].append(val_set["pheno"])
     classifier = model(**params)
-    classifier.fit(training_set["beta"], training_set["pheno"].values.ravel())
-    return classifier, classifier.score(test_set["beta"], test_set["pheno"].values.ravel())
+    classifier.fit(train_set["beta"], train_set["pheno"].values.ravel())
+    return classifier, classifier.score(test_data["beta"], test_data["pheno"].values.ravel())
+
+
+def slice_methylation_array_to_merge(methylation_array, b):
+    return {
+        "pheno": methylation_array["pheno"].rename(index=lambda barcode: "-".join(barcode.split("-")[:4])).loc[b],
+        "beta": methylation_array["beta"].rename(index=lambda barcode: "-".join(barcode.split("-")[:4])).loc[b]
+    }
+
+
+def plot_single_omic_change(scores, ml_method, omic_type, single=True):
+    x, y = [], []
+    x.append(0)
+    y.append(np.mean(scores["base"][omic_type][ml_method]))
+    if single:
+        scope = "single"
+    else:
+        scope = "combined"
+    for std, a in scores[scope][omic_type][ml_method].items():
+        x.append(std)
+        y.append(a)
+
+    plt.figure(figsize=(10, 10))
+    plt.plot(x, y)
+    plt.title("Accuracy of {} adding noise to {}".format(ml_method, omic_type))
+    plt.xlabel("Variance of the noise")
+    plt.ylabel("Accuracy")
+    plt.savefig("../data/results/{}_{}_{}.png".format(ml_method, omic_type, scope))
+
+
+def plot_against_combined(scores, ml_method, omic_type):
+    x_single, y_single = [], []
+    x_combined, y_combined = [], []
+    x_single.append(0)
+    y_single.append(np.mean(scores["base"][omic_type][ml_method]))
+    for std, a in scores["single"][omic_type][ml_method].items():
+        x_single.append(std)
+        y_single.append(a)
+
+    x_combined.append(0)
+    y_combined.append(np.mean(scores["base"]["combined"][ml_method]))
+    for std, a in scores["combined"][omic_type][ml_method].items():
+        x_combined.append(std)
+        y_combined.append(a)
+
+    plt.figure(figsize=(10, 10))
+    plt.plot(x_single, y_single, label="single")
+    plt.plot(x_combined, y_combined, label="combined")
+    plt.title("Accuracy of {} adding noise to {}".format(ml_method, omic_type))
+    plt.xlabel("Variance of the noise")
+    plt.ylabel("Accuracy")
+    plt.legend(loc='best')
+    plt.savefig("../data/results/{}_{}_single_vs_combined.png".format(ml_method, omic_type))
 
 
 # Defining parameters for training
-methylation = True
-mirna = True
-mrna = True
+omics = ["methylation", "mrna", "mirna", "combined"]
 methylation_latent_dimension = 200
 mirna_latent_dimension = 200
 mrna_latent_dimension = 200
@@ -100,35 +151,53 @@ for m in pam50_mirnas:
 pam50_mirnas = list()
 for mirna, genes in mirna_to_genes.items():
     for gene in genes:
-        if gene[1] in pam50_genes:
+        if gene[1] in pam50_genes and gene[2] > 0.8:
             pam50_mirnas.append(mirna)
 
 # PART 2
 # Training the different autoencoders
 # Training the methylation autoencoder
+datasets = dict()
 methylation_dataset = pickle.load(open("../data/final_preprocessed/breast_methylation_450_ma.pkl", "rb"))
 index = set(methylation_dataset["beta"].columns.values)
 pam50_cpgs = index.intersection(pam50_cpgs)
 methylation_dataset["beta"] = methylation_dataset["beta"][pam50_cpgs]
 methylation_encoder = train_autoencoder(methylation_dataset, methylation_latent_dimension)
-methylation_dataset = correct_labels(methylation_dataset)
-methylation_embedded = methylation_encoder.encode_methylation_array(methylation_dataset)
+datasets["methylation"] = {
+    "original": correct_labels(methylation_dataset),
+    "embedded": methylation_encoder.encode_methylation_array(methylation_dataset)
+}
 # Training the mRNA autoencoder
 mrna_dataset = pickle.load(open("../data/mrna_exp_ma.pkl", "rb"))
 mrna_dataset["beta"] = mrna_dataset["beta"].rename(columns=lambda g: g.split('.')[0])
-# mrna_dataset["beta"] = mrna_dataset["beta"][pam50_mrnas]
+mrna_dataset["beta"] = mrna_dataset["beta"][pam50_mrnas]
 mrna_encoder = train_autoencoder(mrna_dataset, mrna_latent_dimension)
-mrna_dataset = correct_labels(mrna_dataset)
-mrna_embedded = mrna_encoder.encode_methylation_array(mrna_dataset)
+datasets["mrna"] = {
+    "original": correct_labels(mrna_dataset),
+    "embedded": mrna_encoder.encode_methylation_array(mrna_dataset)
+}
 # Training the miRNA autoencoder
 mirna_dataset = pickle.load(open("../data/mirna_exp_ma.pkl", "rb"))
 mirna_dataset["beta"] = mirna_dataset["beta"][pam50_mirnas]
 mirna_encoder = train_autoencoder(mirna_dataset, mirna_latent_dimension)
-mirna_dataset = correct_labels(mirna_dataset)
-mirna_embedded = mirna_encoder.encode_methylation_array(mirna_dataset)
+datasets["mirna"] = {
+    "original": correct_labels(mirna_dataset),
+    "embedded": mirna_encoder.encode_methylation_array(mirna_dataset)
+}
 # Combined dataset
-combined_dataset = merge_methylation_arrays(methylation_dataset, mrna_dataset, mirna_dataset)
-combined_embedded = merge_methylation_arrays(methylation_embedded, mrna_embedded, mirna_embedded)
+datasets["combined"] = {
+    "embedded": merge_methylation_arrays(
+        datasets["methylation"]["embedded"],
+        datasets["mrna"]["embedded"],
+        datasets["mirna"]["embedded"]
+    )
+}
+
+encoders = {
+    "methylation": methylation_encoder,
+    "mrna": mrna_encoder,
+    "mirna": mirna_encoder
+}
 
 # PART 3
 # Training the different classifiers
@@ -165,85 +234,135 @@ stats = {
     }
 }
 
-methylation_barcodes, mrna_barcodes, mirna_barcodes, combined_barcodes = [], [], [], []
+barcodes, models = dict(), dict(),
 for i in range(10):
-    # Training models that take as input only methylation
-    methylation_train, methylation_val, methylation_test = \
-        split_methylation_array_by_pheno(methylation_embedded, "subtype", val_rate=0.1, test_rate=0.1)
-    methylation_barcodes = list(methylation_test["pheno"].index.values)
-    # DNN - PAM
-    pam_methylation, dnn_acc = train_dnn_classifier(PAMClassifier,methylation_train, methylation_val, methylation_test)
-    stats["base"]["methylation"]["dnn"].append(dnn_acc)
-    # SVM
-    svm_methylation, svm_acc = train_ml_classifier(methylation_train, methylation_test, SVC, {"C": 1, "kernel": "rbf"},
-                                                   methylation_val)
-    stats["base"]["methylation"]["svm"].append(svm_acc)
-    # KNN
-    knn_methylation, knn_acc = train_ml_classifier(methylation_train, methylation_test, KNeighborsClassifier,
-                                                   {"n_neighbors": 75}, methylation_val)
-    stats["base"]["methylation"]["knn"].append(knn_acc)
-    # RF
-    rf_methylation, rf_acc = train_ml_classifier(methylation_train, methylation_test, RandomForestClassifier,
-                                                 {"n_estimators": 2000, "max_features": "auto"}, methylation_val)
-    stats["base"]["methylation"]["rf"].append(rf_acc)
+    for omic in omics:
+        training_set, validation_set, test_set = \
+            split_methylation_array_by_pheno(datasets[omic]["embedded"], "subtype", val_rate=0.1, test_rate=0.1)
+        barcodes[omic] = list(test_set["pheno"].index.values)
+        models[omic] = dict()
 
-    # Training models that take as input only mRNA expression
-    mrna_train, mrna_val, mrna_test = \
-        split_methylation_array_by_pheno(mrna_embedded, "subtype", val_rate=0.1, test_rate=0.1)
-    mrna_barcodes = list(mrna_test["pheno"].index.values)
-    # DNN
-    dnn_mrna, dnn_acc = train_dnn_classifier(mrna_train, mrna_val, mrna_test)
-    stats["base"]["mrna"]["dnn"].append(dnn_acc)
-    # SVM
-    svm_mrna, svm_acc = train_ml_classifier(mrna_train, mrna_test, SVC, {"C": 1, "kernel": "rbf"}, mrna_val)
-    stats["base"]["mrna"]["svm"].append(svm_acc)
-    # KNN
-    knn_mrna, knn_acc = train_ml_classifier(mrna_train, mrna_test, KNeighborsClassifier, {"n_neighbors": 75}, mrna_val)
-    stats["base"]["mrna"]["knn"].append(knn_acc)
-    # RF
-    rf_mrna, rf_acc = train_ml_classifier(mrna_train, mrna_test, RandomForestClassifier,
-                                          {"n_estimators": 2000, "max_features": "auto"}, mrna_val)
-    stats["base"]["mrna"]["rf"].append(rf_acc)
+        # DNN - PAM
+        models[omic]["pam"], acc = train_dnn_classifier(PAMClassifier, training_set, validation_set, test_set)
+        stats["base"][omic]["pam"].append(acc)
 
-    # Training models that take as input only miRNA expression
-    mirna_train, mirna_val, mirna_test = \
-        split_methylation_array_by_pheno(mirna_embedded, "subtype", val_rate=0.1, test_rate=0.1)
-    mirna_barcodes = list(mirna_test["pheno"].index.values)
-    # DNN
-    dnn_mirna, dnn_acc = train_dnn_classifier(mirna_train, mirna_val, mirna_test)
-    stats["base"]["mirna"]["dnn"].append(dnn_acc)
-    # SVM
-    svm_mirna, svm_acc = train_ml_classifier(mirna_train, mirna_test, SVC, {"C": 1, "kernel": "rbf"}, mirna_val)
-    stats["base"]["mirna"]["svm"].append(svm_acc)
-    # KNN
-    knn_mirna, knn_acc = train_ml_classifier(mirna_train, mirna_test, KNeighborsClassifier, {"n_neighbors": 75},
-                                             mirna_val)
-    stats["base"]["mirna"]["knn"].append(knn_acc)
-    # RF
-    rf_mirna, rf_acc = train_ml_classifier(mirna_train, mirna_test, RandomForestClassifier,
-                                           {"n_estimators": 2000, "max_features": "auto"}, mirna_val)
-    stats["base"]["mirna"]["rf"].append(rf_acc)
+        # DNN - MOLI
+        models[omic]["moli"], acc = train_dnn_classifier(MOLIClassifier, training_set, validation_set, test_set)
+        stats["base"][omic]["moli"].append(acc)
 
-    # Training the combined model
-    combined_train, combined_val, combined_test = \
-        split_methylation_array_by_pheno(combined_embedded, "subtype", val_rate=0.1, test_rate=0.1)
-    combined_barcodes = list(combined_test["pheno"].index.values)
-    # DNN
-    dnn_combined, dnn_acc = train_dnn_classifier(combined_train, combined_val, combined_test)
-    stats["base"]["combined"]["dnn"].append(dnn_acc)
-    # SVM
-    svm_combined, svm_acc = train_ml_classifier(combined_train, combined_test, SVC, {"C": 1, "kernel": "rbf"},
-                                                combined_val)
-    stats["base"]["combined"]["svm"].append(svm_acc)
-    # KNN
-    knn_combined, knn_acc = train_ml_classifier(combined_train, combined_test, KNeighborsClassifier,
-                                                {"n_neighbors": 75}, combined_val)
-    stats["base"]["combined"]["knn"].append(knn_acc)
-    # RF
-    rf_combined, rf_acc = train_ml_classifier(combined_train, combined_test, RandomForestClassifier,
-                                              {"n_estimators": 2000, "max_features": "auto"}, combined_val)
-    stats["base"]["combined"]["rf"].append(rf_acc)
+        # SVM
+        models[omic]["svm"], acc = train_ml_classifier(training_set, test_set, SVC, {"C": 1, "kernel": "rbf"},
+                                                       validation_set)
+        stats["base"][omic]["svm"].append(acc)
 
-for omic, omic_stats in stats["base"].items():
+        # KNN
+        models[omic]["knn"], acc = train_ml_classifier(training_set, test_set, KNeighborsClassifier,
+                                                       {"n_neighbors": 75}, validation_set)
+        stats["base"][omic]["knn"].append(acc)
+
+        # RF
+        models[omic]["rf"], acc = train_ml_classifier(training_set, test_set, RandomForestClassifier,
+                                                      {"n_estimators": 2000, "max_features": "auto"}, validation_set)
+        stats["base"][omic]["rf"].append(acc)
+
+datasets["combined"]["original"] = {
+    "methylation": slice_methylation_array_to_merge(datasets["methylation"]["original"], barcodes["combined"]),
+    "mrna": slice_methylation_array_to_merge(datasets["mrna"]["original"], barcodes["combined"]),
+    "mirna": slice_methylation_array_to_merge(datasets["mirna"]["original"], barcodes["combined"])
+}
+
+for o, omic_stats in stats["base"].items():
     for c, scores in omic_stats.items():
-        print("{} {} mean acc: {} - std {}".format(omic, c, np.mean(scores), np.std(scores)))
+        print("{} {} mean acc: {} - std {}".format(o, c, np.mean(scores), np.std(scores)))
+
+stats["single"] = {
+    "methylation": {
+        "pam": dict(),
+        "moli": dict(),
+        "svm": dict(),
+        "knn": dict(),
+        "rf": dict()
+    },
+    "mrna": {
+        "pam": dict(),
+        "moli": dict(),
+        "svm": dict(),
+        "knn": dict(),
+        "rf": dict()
+    },
+    "mirna": {
+        "pam": dict(),
+        "moli": dict(),
+        "svm": dict(),
+        "knn": dict(),
+        "rf": dict()
+    }
+}
+stats["combined"] = {
+    "methylation": {
+        "pam": dict(),
+        "moli": dict(),
+        "svm": dict(),
+        "knn": dict(),
+        "rf": dict()
+    },
+    "mrna": {
+        "pam": dict(),
+        "moli": dict(),
+        "svm": dict(),
+        "knn": dict(),
+        "rf": dict()
+    },
+    "mirna": {
+        "pam": dict(),
+        "moli": dict(),
+        "svm": dict(),
+        "knn": dict(),
+        "rf": dict()
+    }
+}
+for omic in ["methylation", "mrna", "mirna"]:
+    for sigma in np.linspace(0.01, 100, num=100):
+        beta = datasets[omic]["original"]["beta"].loc[barcodes[omic]]
+        beta = beta + np.random.normal(0, sigma, beta.shape)
+        pheno = datasets[omic]["original"]["pheno"].loc[barcodes[omic]]
+        new_dataset = {"beta": beta, "pheno": pheno}
+        new_embedded = encoders[omic].encode_methylation_array(new_dataset)
+        stats["single"][omic]["pam"][sigma] = models[omic]["pam"]\
+            .evaluate(new_embedded["beta"].to_numpy(), pd.get_dummies(new_embedded["pheno"]["subtype"]).to_numpy())
+        stats["single"][omic]["moli"][sigma] = models[omic]["moli"]\
+            .evaluate(new_embedded["beta"].to_numpy(), pd.get_dummies(new_embedded["pheno"]["subtype"]).to_numpy())
+        stats["single"][omic]["svm"][sigma] = models[omic]["svm"].score(new_embedded["beta"],
+                                                                        new_embedded["pheno"].values.ravel())
+        stats["single"][omic]["knn"][sigma] = models[omic]["knn"].score(new_embedded["beta"],
+                                                                        new_embedded["pheno"].values.ravel())
+        stats["single"][omic]["rf"][sigma] = models[omic]["rf"].score(new_embedded["beta"],
+                                                                      new_embedded["pheno"].values.ravel())
+
+        to_merge = []
+        for o in ["methylation", "mrna", "mirna"]:
+            if o != omic:
+                to_merge.append(encoders[o].encode_methylation_array(datasets["combined"]["original"][o]))
+            else:
+                beta = datasets["combined"]["original"][o]["beta"].iloc[:, :]
+                beta = beta + np.random.normal(0, sigma, beta.shape)
+                to_merge.append(encoders[o].encode_methylation_array({
+                    "beta": beta,
+                    "pheno": datasets["combined"]["original"][o]["pheno"]
+                }))
+
+        new_embedded = merge_methylation_arrays(*to_merge)
+        stats["combined"][omic]["pam"][sigma] = models["combined"]["pam"] \
+            .evaluate(new_embedded["beta"].to_numpy(), pd.get_dummies(new_embedded["pheno"]["subtype"]).to_numpy())
+        stats["combined"][omic]["moli"][sigma] = models["combined"]["moli"] \
+            .evaluate(new_embedded["beta"].to_numpy(), pd.get_dummies(new_embedded["pheno"]["subtype"]).to_numpy())
+        stats["combined"][omic]["svm"][sigma] = \
+            models["combined"]["svm"].score(new_embedded["beta"], new_embedded["pheno"].values.ravel())
+        stats["combined"][omic]["knn"][sigma] = \
+            models["combined"]["knn"].score(new_embedded["beta"], new_embedded["pheno"].values.ravel())
+        stats["combined"][omic]["rf"][sigma] = \
+            models["combined"]["rf"].score(new_embedded["beta"], new_embedded["pheno"].values.ravel())
+
+for omic in ["methylation", "mrna", "mirna"]:
+    for ml in ["pam", "moli", "svm", "knn", "rf"]:
+        plot_against_combined(stats, ml, omic)
