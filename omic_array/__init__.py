45d857b365fd90145bf2de0a7622dfcdd1398883
TestaDiRapa <v.c.pierro@gmail.com>
2020-11-12 19:04:37 +0100

    Added more methods

--- omic_array/__init__.py
+++ omic_array/__init__.py
@@ -1,29 +1,59 @@
 import pickle
 
 
+class OmicArrayInitError(Exception):
+    pass
+
+
 class OmicArray:
     """
     A class to represent the MethylationArray data type. Renamed to OmicArray for clarity
     """
 
-    def __init__(self, filename):
+    def __init__(self, filename=None, beta=None, pheno=None):
         """
         Class constructor.
         :param filename: a .pkl file containing a dict with beta and pheno pandas dataframes
+        :param beta: the beta values to init the object directly
+        :param pheno: the pheno values to init the object directly
         """
-        tmp = pickle.load(open(filename, "rb"))
-        self.beta = tmp["beta"]
-        self.pheno = tmp["pheno"]
+        if filename is None and beta is None and pheno is None:
+            raise OmicArrayInitError("An input source must be specified")
+        elif filename is not None and (beta is not None or pheno is not None):
+            raise OmicArrayInitError("Only one among filename and beta/pheno must be specified")
+        elif (beta is None and pheno is not None) or (beta is not None and pheno is None):
+            raise OmicArrayInitError("Both pheno and beta must be specified")
 
-    def pheno_replace(self, condition, column, value):
+        if filename is not None:
+            tmp = pickle.load(open(filename, "rb"))
+            self.beta = tmp["beta"]
+            self.pheno = tmp["pheno"]
+        else:
+            self.beta = beta
+            self.pheno = pheno
+
+    def deep_copy(self):
+        """
+        Creates a deep copy of the structure
+        :return: an OmicArray
+        """
+        return OmicArray(beta=self.beta.copy(), pheno=self.pheno.copy())
+
+    def pheno_replace(self, condition, column, value, inplace=True):
         """
         Replace values in pheno
         :param condition: a boolean array representing each row in the
         :param column: the column where the values are to be replaced
         :param value: the value to replace
-        :return:
+        :param inplace: boolean value. False if a new OmicArray must be created
+        :return: an OmicArray if inplace is False
         """
-        self.pheno.loc[condition, column] = value
+        if inplace:
+            self.pheno.loc[condition, column] = value
+        else:
+            dc = self.deep_copy()
+            dc.pheno_replace(condition, column, value)
+            return dc
 
     def sklearn_conversion(self, pheno_column):
         """
@@ -32,6 +62,14 @@ class OmicArray:
         :return: X and y
         """
         return self.beta.to_numpy(), self.pheno[pheno_column].ravel()
+
+    def pheno_unique_values(self, pheno_column):
+        """
+        Returns a list containing all the unique values contained into a column of the pheno dataframe
+        :param pheno_column: the column
+        :return: a list
+        """
+        return list(self.pheno[pheno_column].unique())
 
     def __str__(self):
         return "OMIC \n {} values for {} samples \n {} \n PHENO \n {} values for {} samples \n {}".format(
