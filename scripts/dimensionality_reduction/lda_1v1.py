9d26bbd1c01e16c3b4f6ee0d425d62945d4e31aa
TestaDiRapa <v.c.pierro@gmail.com>
2020-12-10 12:34:18 +0100

    Moved plot to function

--- scripts/dimensionality_reduction/lda_1v1.py
+++ scripts/dimensionality_reduction/lda_1v1.py
@@ -1,11 +1,22 @@
 from dataset.dimensionality_reduction import coefficients_by_magnitude
-import matplotlib.pyplot as plt
+from enrichr import enrichr_query
 from omic_array import OmicArray
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.metrics import auc, roc_curve
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 import numpy as np
 import pickle
+
+
+def plot_roc_curve(values):
+    plt.figure()
+    for k, v in values.items():
+        plt.plot(v[0], v[1], label=k)
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.legend(loc="best")
+    plt.show()
 
 
 def lda_feature_selection(omic_array):
@@ -63,7 +74,7 @@ if __name__ == "__main__":
     bm_450 = OmicArray(filename="../../data/breast/methylation/breast_450k_final.pkl")
     bm_27 = OmicArray(filename="../../data/breast/methylation/breast_methylation_27k.pkl")
 
-    dataset_choice = 1
+    dataset_choice = 2
     if dataset_choice == 1:
         dataset = bm_450
     elif dataset_choice == 2:
@@ -72,7 +83,7 @@ if __name__ == "__main__":
         bm_450.select_features_omic(common_index.to_list())
         dataset = bm_450
 
-    dataset_mod = None
+    dataset_mod = "368REV"
     if dataset_mod == "PAM50":
         dataset.select_features_omic(pam50_cpg)
     elif dataset_mod == "368ALL":
@@ -81,7 +92,9 @@ if __name__ == "__main__":
         dataset.select_features_omic(pam50_cpg.union(cpg_368_t))
 
     subtypes = dataset.pheno_unique_values("subtype")
-    to_plot = dict()
+    to_plot_all = dict()
+    to_plot_mag = dict()
+    highest_mag = list()
     for i in range(len(subtypes)):
         subtype_1 = subtypes[i]
         for j in range(i+1, len(subtypes)):
@@ -90,15 +103,26 @@ if __name__ == "__main__":
             work_dataset.filter_classes("subtype", subtype_1, subtype_2)
             work_dataset.scale_features(StandardScaler())
             r = lda_feature_selection(work_dataset)
-            fnr, tpr, a = roc_curve_magnitude(work_dataset, r, subtype_1, subtype_2)
-            to_plot["{} vs {} (AUC = {:.2f}".format(subtype_1, subtype_2, a)] = (fnr, tpr)
 
-    plt.figure()
-    for k, v in to_plot.items():
-        plt.plot(v[0], v[1], label=k)
-    plt.xlabel("False Positive Rate")
-    plt.ylabel("True Positive Rate")
-    plt.legend(loc="best")
-    plt.show()
+            fnr, tpr, a = roc_curve_magnitude(work_dataset, r, subtype_1, subtype_2)
+            to_plot_all["{} vs {} (AUC = {:.2f}".format(subtype_1, subtype_2, a)] = (fnr, tpr)
+
+            highest_mag += r[max(r.keys())]["feats"]
+            fnr, tpr, a = roc_curve_magnitude(work_dataset, r, subtype_1, subtype_2, magnitudes=[max(r.keys())])
+            to_plot_mag["{} vs {} (AUC = {:.2f}".format(subtype_1, subtype_2, a)] = (fnr, tpr)
+
+    plot_roc_curve(to_plot_all)
+    plot_roc_curve(to_plot_mag)
+
+    gene_highest = set()
+    for c in highest_mag:
+        gene_highest = gene_highest.union(cpg_info[c]["genes"])
+
+    library = "KEGG_2019_Human"
+
+    pathways = enrichr_query(list(gene_highest), library)
+    for p in pathways[library][:15]:
+        print(p[1])
+
 
 
